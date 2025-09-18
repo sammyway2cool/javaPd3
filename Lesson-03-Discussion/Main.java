60e68905e5e9d556c2c4ed004974588eb87fb08f
sammyway2cool <samuelm420@nycstudents.net>
2025-09-18 12:57:46 +0000

    .

--- Lesson-03-Discussion/Main.java
+++ Lesson-03-Discussion/Main.java
@@ -10,8 +10,9 @@ class Main {
 
       // This is how to print to the console 
       // in Java.
-      System.out.println("Hello World");
-    
+      System.out.println("Samuel moreno");
+      System.out.println("26 12 168th st");
+      System.out.println("Samuel moreno\n 26 12 168th st");
   }
 
   
