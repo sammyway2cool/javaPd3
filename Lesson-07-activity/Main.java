d784e67293a2ca6bf609c0582799cf03e8c569d0
sammyway2cool <samuelm420@nycstudents.net>
2025-09-25 23:19:55 +0000

    .

--- Lesson-07-activity/Main.java
+++ Lesson-07-activity/Main.java
@@ -22,22 +22,25 @@ System.out.printin(y);
     Challenge 2:
     1) Create the variables, ask the user for the variable values, write the equation in fileEQ1.1-act6 and display the equation value.
 */
-System.out.println("Enter x");
-  double x = Input.readDouble();
-  double result = Math.pow("z,3")+5;
 
-
-  
+System.out.println("Enter in q");
+double z = Input.readDouble();
+double q =0;
+q = Math.pow(z,3)+5;
+System.out.println(y);
 
 /*  
     Challenge 3:
     Create the variables, ask the user for the variable values, write the equation in file EQ2-act6 and display the equation value..
     
 */
-System.out.println("Enter x");
-  double x = Input.readDouble();
-  double result = Math.pow("x,7");
+System.out.println("Enter in s");
+double t = Input.readDouble();
+double r = Input.readDouble();
+double s = 0;
 
+s = Math.pow(t,5) * Math.pow(r+2,4);
+System.out.println(s);
 
  
 
@@ -46,7 +49,12 @@ System.out.println("Enter x");
     Create the variables, ask the user for the variable values, write the equation in file EQ3-act6 and display the equation value..
     
 */
+System.out.println("Enter in C");
+ double A = Input.readDouble();
+ double B = Input.readDouble();
+ double C = 0;
 
+C = Math.sqrt(A)+B;
 
 
 /*  
@@ -54,7 +62,15 @@ System.out.println("Enter x");
     Create the variables, ask the user for the variable values, write the equation in file EQ4-act6 and display the equation value..
     
 */
+System.out.println("Enter in D");
+double x1 = Input.readDouble();
+double y1 = Input.readDouble();
+double x2 = Input.readDouble();
+double y2 = Input.readDouble();
+double d = 0;
 
+d =Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+System.out.println(d);
 
 
 
