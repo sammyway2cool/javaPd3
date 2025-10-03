707c83f442b2ed4e18231fd306b9c1684efe47bf
sammyway2cool <samuelm420@nycstudents.net>
2025-10-03 12:59:17 +0000

    .

---
+++ Lesson-08-Activity/Main.java
@@ -0,0 +1,30 @@
+class Main {
+
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+
+
+}
+String print(string message){ 
+	System.out.printIN(message);
+	return message;
+
+ double flat (double f) {
+	double c = (f-32) * 9 / 5.0 ;
+	return c;
+ }
+
+ double sphereVolume(double r){
+    double v = (4/3.0) * Math.PI * Math.pow(double r, double 3);
+    return v;
+ }
+}
+
+
+
+double coneVolume(double r)
+
+
