ebb77241d77e3c7c4e310a196e56072f32aaf45a
sammyway2cool <samuelm420@nycstudents.net>
2025-10-27 20:56:39 +0000

    .

---
+++ Lesson-15-Activity/Main.java
@@ -0,0 +1,28 @@
+class Main {
+
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init(){
+   
+    
+    
+  }
+  int spaces(int N){
+    string results="";
+    for(int x=0; x<N; x++){
+    return "";
+    }
+
+  }
+
+  int harmonic(int n){
+    
+
+  }
+  
+}
