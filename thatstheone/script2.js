1c6058a73516787323ad92d7de016c1ca2452c12
sammyway2cool <samuelm420@nycstudents.net>
2026-05-31 19:12:57 +0000

    .

--- thatstheone/script2.js
+++ thatstheone/script2.js
@@ -1,24 +1,24 @@
 let data,Members;
 
   async function init(){
-  let link = "https://studious-space-yodel-4jv74vxqqqvg3q56g-8500.app.github.dev";
-  let route= "/Members"
+  let link = "https://reimagined-space-rotary-phone-r464rpr65vw93x9wx-8500.app.github.dev";
+  let route= "/Player"
   info = await fetch(link+route);
   data = await info.json();
   console.log(data);
   generatecards(data);
 }
 
-function generatecards(Members){
+function generatecards(Player){
   let centerpanel = document.getElementById("centerpanel");
   let build ="";
 
-  for(let i=0; i<Members.length; i++){
-    let Member = Members[i]
+  for(let i=0; i<Player.length; i++){
+    let play = Player[i]
     build += `<div class="card" >`
-    build += `<h3> Member Name : ${Member.Name}</h3>`;
-    build += `<div> amount of games bought  : ${Member.Numbergamebought}</div>`;
-    build += `<div> Age : ${Member.Age}</div>`;
+    build += `<h3> Member Name : ${play.Players}</h3>`;
+    build += `<div> amount of games bought  : ${play.Clubs}</div>`;
+    build += `<div> Age : ${play.MaxContracts}</div>`;
     build += `<div> Payment type  : ${Member.Paymenttype}</div>`;
     build += `<p> Email : ${Member.Gmail}</p>`;
     build += `<hr>`;
